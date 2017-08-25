8df906253bd37411957286eb9bf8bbddcee32efd
Airton Júnior <pulquerio.junior@gmail.com>
2017-08-24 21:32:10 -0400

    Enviando o Primeira Versão do Sistema

---
+++ WebAppAula2Lab2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppAula2Lab2.MvcApplication" Language="C#" %>
---
+++ WebAppAula2Lab2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppAula2Lab2.Startup))]
+namespace WebAppAula2Lab2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
